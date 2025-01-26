f703c05b601facb8cf1bfcc8d9ce97c2d83121c1
Julien Doutre <jul.doutre@gmail.com>
2025-01-26 23:14:03 +0100

    insert jwts in db

--- internal/server/record.go
+++ internal/server/record.go
@@ -2,22 +2,37 @@ package server
 
 import (
 	"context"
+	"encoding/json"
 
 	v1 "github.com/juliendoutre/recorder/pkg/v1"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 func (s *Server) Record(ctx context.Context, input *v1.RecordInput) (*emptypb.Empty, error) {
 	token, err := s.jwtParser.Parse(input.GetJwt(), s.jwkStore.Keyfunc)
 	if err != nil {
-		// TODO: return error
+		return nil, status.Error(codes.InvalidArgument, "TODO")
 	}
 
 	if !token.Valid {
-		// TODO: return error
+		return nil, status.Error(codes.Unauthenticated, "TODO")
 	}
 
-	// TODO: insert to DB
+	headers, err := json.Marshal(token.Header)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "TODO")
+	}
+
+	payload, err := json.Marshal(token.Claims)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "TODO")
+	}
+
+	if _, err := s.pg.Exec(ctx, "INSERT INTO recorder.claims (header, payload) VALUES ($1, $2);", headers, payload); err != nil {
+		return nil, status.Error(codes.Internal, "TODO")
+	}
 
 	return &emptypb.Empty{}, nil
 }
--- sql/20250125235926_claims.up.sql
+++ sql/20250125235926_claims.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS recorder.claims (
-    header JSONB NOT NULL,
+    headers JSONB NOT NULL,
     payload JSONB NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
