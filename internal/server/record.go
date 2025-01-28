dca7f36e95c10aabbc7d79f00c5394eea388fcbd
Julien Doutre <jul.doutre@gmail.com>
2025-01-28 19:22:14 +0100

    Input digest

--- internal/server/record.go
+++ internal/server/record.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strings"
 
 	v1 "github.com/juliendoutre/recorder/pkg/v1"
 	"google.golang.org/grpc/codes"
@@ -11,10 +12,22 @@ import (
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
+const digestSize = 71
+
 func (s *Server) Record(ctx context.Context, input *v1.RecordInput) (*emptypb.Empty, error) {
+	digest := input.GetDigest()
+
+	if !strings.HasPrefix(digest, "sha256:") {
+		return nil, status.Error(codes.InvalidArgument, "invalid digest prefix") //nolint:wrapcheck
+	}
+
+	if len(digest) != digestSize {
+		return nil, status.Error(codes.InvalidArgument, "invalid digest length") //nolint:wrapcheck
+	}
+
 	token, err := s.jwtParser.Parse(input.GetJwt(), s.jwkStore.Keyfunc)
 	if err != nil {
-		return nil, status.Error(codes.InvalidArgument, fmt.Errorf("parsing JWT: %w", err).Error()) //nolint:wrapcheck
+		return nil, status.Error(codes.InvalidArgument, fmt.Sprintf("parsing JWT: %s", err)) //nolint:wrapcheck
 	}
 
 	if !token.Valid {
@@ -23,20 +36,20 @@ func (s *Server) Record(ctx context.Context, input *v1.RecordInput) (*emptypb.Em
 
 	headers, err := json.Marshal(token.Header)
 	if err != nil {
-		return nil, status.Error(codes.Internal, fmt.Errorf("marshalling headers: %w", err).Error()) //nolint:wrapcheck
+		return nil, status.Error(codes.Internal, fmt.Sprintf("marshalling headers: %s", err)) //nolint:wrapcheck
 	}
 
 	payload, err := json.Marshal(token.Claims)
 	if err != nil {
-		return nil, status.Error(codes.Internal, fmt.Errorf("marshalling payload: %w", err).Error()) //nolint:wrapcheck
+		return nil, status.Error(codes.Internal, fmt.Sprintf("marshalling payload: %s", err)) //nolint:wrapcheck
 	}
 
 	if _, err := s.pg.Exec(
 		ctx,
-		"INSERT INTO recorder.claims (header, payload) VALUES ($1, $2);",
-		headers, payload,
+		"INSERT INTO recorder.claims (digest, header, payload) VALUES ($1, $2, $3);",
+		digest, headers, payload,
 	); err != nil {
-		return nil, status.Error(codes.Internal, fmt.Errorf("inserting record: %w", err).Error()) //nolint:wrapcheck
+		return nil, status.Error(codes.Internal, fmt.Sprintf("inserting record: %s", err)) //nolint:wrapcheck
 	}
 
 	return &emptypb.Empty{}, nil
--- sql/20250125235926_claims.up.sql
+++ sql/20250125235926_claims.up.sql
@@ -1,4 +1,5 @@
 CREATE TABLE IF NOT EXISTS recorder.claims (
+    digest TEXT NOT NULL,
     headers JSONB NOT NULL,
     payload JSONB NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
